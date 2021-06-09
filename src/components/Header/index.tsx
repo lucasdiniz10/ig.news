50ab7398d1db7be9e855e29f3dd9c2e6e955af73
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2021-06-09 18:46:17 -0300

    header component estrutura inicial

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,15 @@
+import styles from './styles.module.scss';
+
 export function Header() {
   return (
-    <header>
-
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
     </header>
   );
 }
