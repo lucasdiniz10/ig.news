41896147f5835129783c27debb96fa675bc8898c
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2021-06-16 20:54:33 -0300

    estrutura inicial home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,20 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Hello World
-      </h1>
+      <main>
+        <section>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   )
 }
