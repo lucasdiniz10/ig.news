713b629e9946d7f2e1b91c5171df285bb5ce8d28
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2021-06-20 19:14:31 -0300

    props SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home({ product }: HomeProps) {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
           <SubscribeButton />
         </section>
