9fa84cb9fcb87e0253e98516cde9e45bb9115025
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2021-06-20 19:19:08 -0300

    preco formatado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,7 +43,10 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
   const product = {
     priceId: price.id,
-    amount: price.unit_amount / 100,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
   }
 
   return {
