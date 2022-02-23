bb01880524fc1cdc2c38c57e1c23f8633602ea2c
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2022-02-23 18:29:07 -0300

    listando posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,13 +1,24 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
 import { getPrismicClient } from '../../services/prismic';
 
 import styles from './styles.module.scss';
-import { query } from 'faunadb';
 
-export default function Posts() {
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string;
+}
+
+interface PostsProps {
+    posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
     return (
         <>
             <Head>
@@ -16,27 +27,13 @@ export default function Posts() {
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a href="#">
-                        <time>21 de Fevereiro de 2022</time>
-                        <strong>JAMstack: geleia de JavaScript, API e Markup</strong>
-                        <p>
-                            O que significa e por que é considerada uma arquitetura moderna de desenvolvimento web? Uma arquitetura moderna de desenvolvimento, vista como vanguarda na renascença de web sites estáticos, e com nome de um doce popularmente conhecido como GELEIA.
-                        </p>
-                    </a>
-                    <a href="#">
-                        <time>21 de Fevereiro de 2022</time>
-                        <strong>JAMstack: geleia de JavaScript, API e Markup</strong>
-                        <p>
-                            O que significa e por que é considerada uma arquitetura moderna de desenvolvimento web? Uma arquitetura moderna de desenvolvimento, vista como vanguarda na renascença de web sites estáticos, e com nome de um doce popularmente conhecido como GELEIA.
-                        </p>
-                    </a>
-                    <a href="#">
-                        <time>21 de Fevereiro de 2022</time>
-                        <strong>JAMstack: geleia de JavaScript, API e Markup</strong>
-                        <p>
-                            O que significa e por que é considerada uma arquitetura moderna de desenvolvimento web? Uma arquitetura moderna de desenvolvimento, vista como vanguarda na renascença de web sites estáticos, e com nome de um doce popularmente conhecido como GELEIA.
-                        </p>
-                    </a>
+                    {posts.map(post => (
+                        <a key={post.slug} href="#">
+                            <time>{post.updatedAt}</time>
+                            <strong>{post.title}</strong>
+                            <p>{post.excerpt}</p>
+                        </a>
+                    ))}
                 </div>
             </main>
         </>
@@ -46,16 +43,29 @@ export default function Posts() {
 export const getStaticProps: GetStaticProps = async () => {
     const prismic = getPrismicClient()
 
-    const response = await prismic.query([
+    const response = await prismic.query<any>([
         Prismic.predicates.at('document.type', 'publication')
     ], {
         fetch: ['publication.title', 'publication.content'],
         pageSize: 100,
     })
 
-    console.log(JSON.stringify(response, null, 2));
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric',
+            })
+        }
+    })
 
     return {
-        props: {}
+        props: {
+            posts
+        }
     }
 }
